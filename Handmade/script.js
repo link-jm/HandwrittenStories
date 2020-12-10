d6565d13db5795bc8f37ec601c5e343e4c7f2279
link-jm <58797201+link-jm@users.noreply.github.com>
2020-12-10 16:43:36 -0600

    Handwritten colors
    Override colors and add Handwritten Stories colors

--- Handmade/script.js
+++ Handmade/script.js
@@ -1,5 +1,5 @@
-console.log('Hey there, curious developer! I see you peeking ;)');
+console.log('Thank you Stephjs for this awesome tutorial on how to change your tinyurl for free. You rock!');
 
 console.log(
-	'Feel free to check out all the code on my github: https://github.com/stephjs'
+	'If you want to know more for the original source code go to this github: https://github.com/stephjs'
 );
